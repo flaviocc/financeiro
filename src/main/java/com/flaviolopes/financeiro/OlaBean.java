765b68b3717e10e5533bdbb03d2145230d27952f
Flavio Lopes <flaviolopes.cc@gmail.com>
2020-07-25 01:29:38 -0300

    adicionado dependencias e arquivos de configuracoes do jsf

---
+++ src/main/java/com/flaviolopes/financeiro/OlaBean.java
@@ -0,0 +1,36 @@
+package com.flaviolopes.financeiro;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaBean {
+
+	private String nome;
+	private String sobreNome;
+	private String nomeCompleto;
+
+	public void dizerOla() {
+		this.nomeCompleto = this.nome.toUpperCase() + " " + this.sobreNome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+}
